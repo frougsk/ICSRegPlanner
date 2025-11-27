5796b3d5bd3588e164a41b527c1ce074f63cd7de
Huxlotyl <165471580+Huxlotyl@users.noreply.github.com>
2025-11-27 17:15:29 +0800

    Added hashmap for added courses

--- src/bases/Account.java
+++ src/bases/Account.java
@@ -1,5 +1,7 @@
 package bases;
 
+import java.util.LinkedHashMap;
+
 public class Account {
 	// Attributes
     private final String emailAddress;
@@ -8,6 +10,9 @@ public class Account {
     private final String lastName;
     private final String program; 
     private final String password; 
+    
+    private final LinkedHashMap<String, Offering> basketCourses;
+//    private final LinkedHashMap<String, Offering> enrolledCourses;
 
     // Constructor
     public Account(String emailAddress, String firstName,
@@ -18,15 +23,33 @@ public class Account {
         this.lastName = lastName;
         this.program = userType;
         this.password = password;
+        
+        this.basketCourses = new LinkedHashMap<>();
+//        this.enrolledCourses = new LinkedHashMap<>();
     }
 
     // Getters
     public String getEmailAddress() { return emailAddress; }
-    public String getFirstName()    { return firstName; }
-    public String getMiddleName()   { return middleName; }
-    public String getLastName()     { return lastName; }
-    public String getProgram()     { return program; }
-    public String getPassword()     { return password; }
+    public String getFirstName() { return firstName; }
+    public String getMiddleName() { return middleName; }
+    public String getLastName() { return lastName; }
+    public String getProgram() { return program; }
+    public String getPassword() { return password; }
+    public LinkedHashMap<String, Offering> getBasket() { return basketCourses;}
+//    public LinkedHashMap<String, Offering> getEnrolledCourses() { return enrolledCourses;}
+    
+    // Course Operations
+//    public void enrollCourse(Offering offering) {
+//        enrolledCourses.put(offering.getCode(), offering);
+//    }
+    
+    public void addToBasket(Offering offering) {
+        basketCourses.put(offering.getCode(), offering);
+    }
+    
+    public void removeFromBasket(String courseCode) {
+        basketCourses.remove(courseCode); 
+    }
 
 	//Converts to file format
 	public String toFile() {
@@ -34,5 +57,6 @@ public class Account {
 	}
 	
 }
+
 
 
