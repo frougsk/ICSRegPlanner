80ab2a030439ca25e2124636318de51b1338c54c
yanyanyan06 <lehidalgo@up.edu.ph>
2025-12-03 04:48:00 +0800

    Update Planner_SearchAdd.java
    updated planner_searchAdd

--- src/application/Planner_SearchAdd.java
+++ src/application/Planner_SearchAdd.java
@@ -19,24 +19,17 @@ import java.util.function.Consumer;
 
 public class Planner_SearchAdd {
 	
-	private final Account account;
-	private StackPane root;// = new StackPane() ;
+	private StackPane root = new StackPane() ;
 	private Consumer<Offering> addToBasket;
-	private FilteredList<Offering> filtered;
+	private FilteredList<Offering> searchList;
 	
-	public Planner_SearchAdd(Account account) {
-		this.account = account;
-		this.root  = new StackPane();
-		searchAdd();
-	}
-	
-	private void searchAdd() {
+	public Planner_SearchAdd(Account acc, Planner_Sched top) { 
 		//=========== LOAD OFFERED COURSES  ===========
 		OfferingLoader loader = new OfferingLoader();
 		YearSet ay = loader.getOfferings(Paths.get("data/course_offerings.csv"));
 		ObservableList<Offering> offerings = FXCollections.observableArrayList(ay.getOfferings());
-		
-		FilteredList<Offering> searchList = new FilteredList<>(offerings, s -> true);
+				
+		searchList = new FilteredList<>(offerings, s -> true);
 		
 		
 		//=========== TABLEVIEW  ===========
@@ -46,7 +39,7 @@ public class Planner_SearchAdd {
         
 		TableColumn<Offering, String> colCode   = new TableColumn<>("Course Code");
         TableColumn<Offering, String> colTitle      = new TableColumn<>("Course Title");
-        TableColumn<Offering, String> colUnit  = new TableColumn<>("Units");
+        TableColumn<Offering, Integer> colUnit  = new TableColumn<>("Units");
         TableColumn<Offering, String> colSec = new TableColumn<>("Section");
         TableColumn<Offering, String> colTime   = new TableColumn<>("Times");
         TableColumn<Offering, String> colDay   = new TableColumn<>("Days");
@@ -78,32 +71,35 @@ public class Planner_SearchAdd {
 			searchCourse(searchList, codeSearch.getText(), titleSearch.getText(), table);
 			});
 		
-		HBox search = new HBox(10, codeSearch, titleSearch);
-        search.setAlignment(Pos.TOP_LEFT);
-        
+		       
         
         //=========== ADD COURSE  ===========       
-        Button add = new Button("Add");
+        Button add = new Button("Add to Basket");
         add.setOnMouseClicked(e -> {
         	Offering selected = table.getSelectionModel().getSelectedItem();
         	if(selected != null && addToBasket != null) {
         		addToBasket.accept(selected);
+        	} else if (selected == null) {
+        		top.error("[ERROR] No course are selected");
         	}
         });   
         
         
         // =========== LAYOUT ===========
+        HBox search = new HBox(10, codeSearch, titleSearch, add);
+        search.setAlignment(Pos.TOP_LEFT);
+        
         Label seachLbl = new Label("COURSE SEARCH");
         seachLbl.getStyleClass().add("hello-style");
         seachLbl.setAlignment(Pos.TOP_LEFT);
         
-        VBox searchAdd = new VBox(10, search, table, add);
-        //searchAdd.setAlignment(Pos.CENTER);
+        VBox searchAdd = new VBox(10, seachLbl, search, table);
+        searchAdd.setAlignment(Pos.CENTER);
         searchAdd.setPadding(new Insets(10, 80, 10, 80));
-        searchAdd.setMaxHeight(Double.MAX_VALUE);
+        searchAdd.setMinHeight(575);
         searchAdd.getStyleClass().add("login-box");
         
-        root.getChildren().add(searchAdd);
+        root = new StackPane(searchAdd);
         }
 	    
     public Node getNode() {return root; }
@@ -127,5 +123,7 @@ public class Planner_SearchAdd {
 			return codeMatched && titleMatched;
 		});
 	}
+	
+	
+	
 }
-
