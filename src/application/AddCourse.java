b289a3243398560dfa2f69ba2cbb56206c35e8cf
yanyanyan06 <lehidalgo@up.edu.ph>
2025-12-03 05:11:22 +0800

    Update AddCourse.java
    added warning messages
    updated toMinute() method
    added instance for multiple lab class

--- src/application/AddCourse.java
+++ src/application/AddCourse.java
@@ -1,31 +1,70 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bases.Account;
 import bases.Offering;
 
 public class AddCourse {
+	
+    public static boolean addCourse(Account account, Offering newCourse, Planner_Sched top) {
+    	top.setWarning(null);
+    	String newCode = newCourse.getCode().trim().toUpperCase();
+        String newSection = newCourse.getSection().trim().toUpperCase();
+        
+        for (Offering existing: account.getBasket().values()) {
+        	// Checks for exact duplicate of course and section
+        	if (existing.getCode().equalsIgnoreCase(newCourse.getCode()) &&
+        			existing.getSection().equalsIgnoreCase(newCourse.getSection())) {
+				if (true) {
+        		
+        		// Check if possible duplicate course (except CMSC 290 and CMSC 291
+	            /**if (!newCourse.getCode().equalsIgnoreCase("CMSC 290") &&
+	                !newCourse.getCode().equalsIgnoreCase("CMSC 291")) {**/
+	            		            	
+					top.error("[ERROR] " + newCourse.getCode() + " " + newCourse.getSection() + " is already in your basket");
+	                return false;
+	            }
+	    	}
+    	}       
 
-    public static boolean addCourse(Account account, Offering newCourse) {
-        // Check if possible duplicate course (except CMSC 290 and CMSC 291)
-        if (account.getBasket().containsKey(newCourse.getCode())) {
-            if (!newCourse.getCode().equalsIgnoreCase("CMSC 290") &&
-                !newCourse.getCode().equalsIgnoreCase("CMSC 291")) {
-                // INSERT CODE TO PROMPT USER THAT THIS ACTION CANNOT BE DONE
+        List<Offering> withSameCode = new ArrayList<>();
+        for (Offering existing : account.getBasket().values()) {
+            if (existing.getCode().trim().toUpperCase().equals(newCode)) {
+            	withSameCode.add(existing);
+            }
+                
+            if (withSameCode.size() >= 2) {
+            	top.error("[ERROR] You can only add a Lecture and Lab Class");
+            }
+        }
+        
+        if (withSameCode.size() == 1) {
+            Offering existing = withSameCode.get(0);
+            
+            boolean existingLab = existing.getSection().toUpperCase().contains("L");
+            boolean newIsLab = newSection.contains("L");
+
+            if (existingLab == newIsLab) {
+                top.error("[ERROR] You already have a Lab Class.");
                 return false;
             }
         }
-
+    
         // Check for schedule conflicts (True if overlap)
         for (Offering existing : account.getBasket().values()) {
             if (conflicts(existing, newCourse)) {
-            	// INSERT CODE TO PROMPT USER THAT THIS ACTION CANNOT BE DONE
-                return false;
+            	 top.error("[ERROR] " + newCourse.getCode() + " overlaps with your added courses");
+            	return false;
             }
         }
 
         // Add to basket if possible
         account.addToBasket(newCourse);
-        // INSERT CODE TO PROMPT USER THAT ACTION WAS DONE SUCCESSFULLY
+        top.success("[SUCCESS] Successfully added " + newCourse.getCode());
+       
+		
         return true;
     }
 
@@ -50,10 +89,32 @@ public class AddCourse {
     }
 
     // Helper Method: Convert "HH:MM" to minutes since midnight
-    private static int toMinutes(String time) {
-        String[] parts = time.trim().split(":");
+    static int toMinutes(String time) {
+        time = time.trim();
+        
+        // Split into hours and minutes
+        String[] parts = time.split(":");
+        
+        if (parts.length < 2) {
+             try {
+                 int hour = Integer.parseInt(time);
+                 parts = new String[] {String.valueOf(hour), "00"};
+             } catch (NumberFormatException e) {
+                 return 0; 
+             }
+        }
+
         int hour = Integer.parseInt(parts[0]);
         int min  = Integer.parseInt(parts[1]);
+      
+        // Handles the 12:00 format to accomodate 24:00 format
+        if (hour >= 1 && hour <= 6) { 
+            hour += 12; 
+        } else if (hour >= 1 && hour <= 11 && hour != 12) {
+        	if (hour >= 1 && hour <= 7) { 
+                hour += 12; // Converts 1 PM to 13, 7 PM to 19
+        	}
+        } 
         return hour * 60 + min;
     }
 }
