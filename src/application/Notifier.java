91f542924c9acdc3ec380c28a3cdf19b9ce9f445
VEN <jbcalleja@up.edu.ph>
2025-12-03 20:15:07 +0800

    added more messages

--- src/application/Notifier.java
+++ src/application/Notifier.java
@@ -59,4 +59,139 @@ public class Notifier {
 	    popup.setResizable(false);
 	    popup.showAndWait();
 	}
+	
+	public static void missingInput() {
+	    Stage popup = new Stage();
+	    popup.setTitle("Error!");
+
+	    // stops main window be accessed when an error is present
+	    popup.initModality(Modality.APPLICATION_MODAL);
+
+	    VBox box = new VBox(0);
+	    box.setAlignment(Pos.CENTER);
+	    box.setPadding(new Insets(30, 20, 40, 20));
+
+	    Label message = new Label("Warning!");
+	    message.getStyleClass().add("error-message");
+	    message.setStyle("-fx-padding: 0;");
+
+	    Label subText = new Label("Please fill all required fields.");
+	    subText.getStyleClass().add("error-message");
+	    subText.setStyle("-fx-font-size: 36px; -fx-font-style: italic; -fx-text-fill: #395c06; -fx-padding: 0;");
+
+	    Button sorry = new Button("Sorry!");
+	    sorry.getStyleClass().add("error-button");
+	    sorry.setOnMouseClicked(e1 -> popup.close());
+
+	    Image lay = new Image(Login.class.getResource("/assets/lay.png").toExternalForm());
+	    ImageView layView = new ImageView(lay);
+	    layView.setPreserveRatio(true);
+	    layView.setFitWidth(290);
+	    layView.setLayoutX(200);
+	    layView.setLayoutY(-120);
+	    layView.setStyle("-fx-cursor: hand;");
+
+	    Pane smiskiPane = new Pane();
+	    smiskiPane.setPrefHeight(70);
+	    smiskiPane.getChildren().add(layView);
+	    smiskiPane.setMouseTransparent(true);
+
+	    box.getChildren().addAll(message, subText, sorry, smiskiPane);
+
+	    Scene popupScene = new Scene(box, 500, 250);
+	    popupScene.getStylesheets().add(Notifier.class.getResource("application.css").toExternalForm());
+	    popup.setScene(popupScene);
+	    popup.setResizable(false);
+	    popup.showAndWait();
+	}
+	
+	public static void passwordNotMatch() {
+	    Stage popup = new Stage();
+	    popup.setTitle("Error!");
+
+	    // stops main window be accessed when an error is present
+	    popup.initModality(Modality.APPLICATION_MODAL);
+
+	    VBox box = new VBox(0);
+	    box.setAlignment(Pos.CENTER);
+	    box.setPadding(new Insets(30, 20, 40, 20));
+
+	    Label message = new Label("Error!");
+	    message.getStyleClass().add("error-message");
+	    message.setStyle("-fx-padding: 0;");
+
+	    Label subText = new Label("Password does not match!");
+	    subText.getStyleClass().add("error-message");
+	    subText.setStyle("-fx-font-size: 36px; -fx-font-style: italic; -fx-text-fill: #395c06; -fx-padding: 0;");
+
+	    Button sorry = new Button("Sorry!");
+	    sorry.getStyleClass().add("error-button");
+	    sorry.setOnMouseClicked(e1 -> popup.close());
+
+	    Image lay = new Image(Login.class.getResource("/assets/lay.png").toExternalForm());
+	    ImageView layView = new ImageView(lay);
+	    layView.setPreserveRatio(true);
+	    layView.setFitWidth(290);
+	    layView.setLayoutX(200);
+	    layView.setLayoutY(-120);
+	    layView.setStyle("-fx-cursor: hand;");
+
+	    Pane smiskiPane = new Pane();
+	    smiskiPane.setPrefHeight(70);
+	    smiskiPane.getChildren().add(layView);
+	    smiskiPane.setMouseTransparent(true);
+
+	    box.getChildren().addAll(message, subText, sorry, smiskiPane);
+
+	    Scene popupScene = new Scene(box, 500, 250);
+	    popupScene.getStylesheets().add(Notifier.class.getResource("application.css").toExternalForm());
+	    popup.setScene(popupScene);
+	    popup.setResizable(false);
+	    popup.showAndWait();
+	}
+
+	public static void successfullyCreated() {
+	    Stage popup = new Stage();
+	    popup.setTitle("Error!");
+
+	    // stops main window be accessed when an error is present
+	    popup.initModality(Modality.APPLICATION_MODAL);
+
+	    VBox box = new VBox(0);
+	    box.setAlignment(Pos.CENTER);
+	    box.setPadding(new Insets(30, 20, 40, 20));
+
+	    Label message = new Label("Success!");
+	    message.getStyleClass().add("error-message");
+	    message.setStyle("-fx-padding: 0;");
+
+	    Label subText = new Label("Account Successfuly Created!");
+	    subText.getStyleClass().add("error-message");
+	    subText.setStyle("-fx-font-size: 36px; -fx-font-style: italic; -fx-text-fill: #395c06; -fx-padding: 0;");
+
+	    Button sorry = new Button("Yay!");
+	    sorry.getStyleClass().add("error-button");
+	    sorry.setOnMouseClicked(e1 -> popup.close());
+
+	    Image lay = new Image(Login.class.getResource("/assets/yay.png").toExternalForm());
+	    ImageView layView = new ImageView(lay);
+	    layView.setPreserveRatio(true);
+	    layView.setFitWidth(290);
+	    layView.setLayoutX(-50);
+	    layView.setLayoutY(-120);
+	    layView.setStyle("-fx-cursor: hand;");
+
+	    Pane smiskiPane = new Pane();
+	    smiskiPane.setPrefHeight(70);
+	    smiskiPane.getChildren().add(layView);
+	    smiskiPane.setMouseTransparent(true);
+
+	    box.getChildren().addAll(message, subText, sorry, smiskiPane);
+
+	    Scene popupScene = new Scene(box, 500, 250);
+	    popupScene.getStylesheets().add(Notifier.class.getResource("application.css").toExternalForm());
+	    popup.setScene(popupScene);
+	    popup.setResizable(false);
+	    popup.showAndWait();
+	}
 }
