4b48ea288034f1a29be7b9fbbc0340b9386671a9
yanyanyan06 <lehidalgo@up.edu.ph>
2025-12-04 22:05:24 +0800

    transferred warning panel to BasketView and updated layout

--- src/planning/Planner_BasketView.java
+++ src/planning/Planner_BasketView.java
@@ -3,8 +3,12 @@ package planning;
 // Package Imports
 import bases.*;
 import fileHandlers.*;
+import application.AddCourse;
+import application.Fonts;
 
-// JavaFX Imports
+//JavaFX Imports
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.*;
 import javafx.collections.transformation.FilteredList;
 import javafx.geometry.Insets;
@@ -12,73 +16,143 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import planning.Planner_SearchAdd.*;
 
 // Java Imports
 import java.nio.file.Paths;
 import java.util.function.Consumer;
 
+
 public class Planner_BasketView {
 	private StackPane root = new StackPane();
 	private TableView<Offering> table = new TableView<>();
 	private Consumer<Offering> removeToBasket;
+	private Label warnings;
 	
 	public Planner_BasketView(Account acc) {
 		//=========== TABLEVIEW  ===========
 		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        table.getStyleClass().add("course-table");
-        
-		TableColumn<Offering, String> colCode   = new TableColumn<>("Course Code");
-        TableColumn<Offering, String> colTitle      = new TableColumn<>("Course Title");
-        TableColumn<Offering, Integer> colUnit  = new TableColumn<>("Units");
-        TableColumn<Offering, String> colSec = new TableColumn<>("Section");
-        TableColumn<Offering, String> colTime   = new TableColumn<>("Times");
-        TableColumn<Offering, String> colDay   = new TableColumn<>("Days");
-        TableColumn<Offering, String> colRoom   = new TableColumn<>("Rooms");
-        
-        colCode.setCellValueFactory(new PropertyValueFactory<>("code"));
-        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
-        colUnit.setCellValueFactory(new PropertyValueFactory<>("units"));
-        colSec.setCellValueFactory(new PropertyValueFactory<>("section"));
-        colTime.setCellValueFactory(new PropertyValueFactory<>("time"));
-        colDay.setCellValueFactory(new PropertyValueFactory<>("day"));
-        colRoom.setCellValueFactory(new PropertyValueFactory<>("room"));
-
-        table.getColumns().addAll(colCode, colTitle, colUnit, colSec, colTime, colDay, colRoom);
-        
-        
-        //=========== DELETE COURSE  ===========
-        Button del = new Button("Delete");
-        del.setOnMouseClicked(e -> {
-        	Offering selected = table.getSelectionModel().getSelectedItem();
-        	if(selected != null && removeToBasket != null) {
-        		removeToBasket.accept(selected);
-        	}
+        table.setRowFactory(tv -> {
+            TableRow<Offering> row = new TableRow<>();
+            row.setPrefHeight(80); 
+            return row;
         });
-         
-        // =========== LAYOUT ===========
-        Label basketLbl = new Label("YOUR BASKET ");
-        basketLbl.getStyleClass().add("hello-style");
-        basketLbl.setAlignment(Pos.TOP_LEFT);
+		
+        TableColumn<Offering, Offering> colDetails = new TableColumn<>("BASKET");
+        colDetails.setPrefWidth(500);
+        colDetails.setCellValueFactory(c ->
+                new SimpleObjectProperty<>(c.getValue()));
+
+        colDetails.setCellFactory(col -> new TableCell<Offering, Offering>() {
+            @Override
+            protected void updateItem(Offering offeringItem, boolean empty) {
+                super.updateItem(offeringItem, empty);
+
+                if (empty || offeringItem == null) {
+                    setGraphic(null);
+                    return;
+                }
+                VBox card = buildCard(offeringItem, offeringItem.getCode() + " " + offeringItem.getSection()); 
+  
+
+                // Remove Course from Basket
+                Button deleteBtn = new Button("Delete");
+                deleteBtn.setMinWidth(60);
+                
+                deleteBtn.setOnAction(e -> {
+                    if (removeToBasket != null) {
+                        removeToBasket.accept(offeringItem);                        
+                    }
+                });
+
+                HBox outer = new HBox(50);
+                outer.setPadding(new Insets(6));
+                outer.setAlignment(Pos.CENTER_LEFT);             
+                outer.getChildren().addAll(card, deleteBtn);
+
+                setGraphic(outer);
+            }
+        });
+        table.getColumns().addAll(colDetails);
+    
+              
+     // =========== WARNING PANEL ===========
+		Text warn = new Text("WARNINGS / BASKET");
+		warn.getStyleClass().add("hello-style");
+		
+		warnings = new Label("-- All Clear --");
+		warnings.setWrapText(true);
+		warnings.setFont(Fonts.loadCrimson(18));
         
-        VBox basket = new VBox(10, basketLbl, table, del);
-        basket.setAlignment(Pos.TOP_CENTER);
-        basket.setMinHeight(500);
-        basket.setPadding(new Insets(10, 80, 10, 80));
-        basket.getStyleClass().add("login-box");
+		
+        // =========== LAYOUT ===========              
+        VBox warnPanel = new VBox(10, warn, warnings);
+		warnPanel.setAlignment(Pos.TOP_LEFT);
+		warnPanel.setMinWidth(250);
+		
+		VBox warnBasket = new VBox(20, warnPanel, table);
+		warnBasket.setAlignment(Pos.CENTER);
+		warnBasket.setPadding(new Insets(10, 5, 0, 5));
+		//warnBasket.getStyleClass().add("login-box");
         
-        root.getChildren().add(basket); 
+        root.getChildren().add(warnBasket); 
         refresh(acc); 
 	}
 	
 	public Node getNode() {return root; }
-
+			
+	// =========== BASKET ESSENTIALS ===========
     public void setRemoveToBasket(Consumer<Offering> handler) {
         this.removeToBasket = handler;
     }
 	
 	public void refresh(Account acc) {
 		table.setItems(FXCollections.observableArrayList(acc.getBasket().values())); 
+	}
+	
+	// =========== DETAILS CARD ===========
+	private VBox buildCard(Offering o, String type) {
+        VBox box = new VBox(6);
+        box.setPadding(new Insets(10));
+        box.setMinWidth(135);
+        box.setStyle("""
+                -fx-background-color: #e8f0ff;
+                -fx-background-radius: 8;
+                -fx-border-radius: 8;
+                -fx-border-color: #4a85ff;
+                """);
+
+        Label typeLbl = new Label(type + " (" + o.getUnits() + " units)");
+        typeLbl.setStyle("-fx-font-weight: bold; -fx-text-fill: #0044aa;");
+        Label room = new Label("Location: " + safeString(o.getRoom()));
+       
+
+        box.getChildren().addAll(typeLbl, room);
+        return box;
+    }
+	
+    private String safeString(String s) {
+        return s == null ? "" : s;
+    }
+	
+    // =========== WARNING PROMPTS ===========
+	public void error(String msg) {
+		warnings.setText(msg);
+		warnings.setTextFill(Color.DARKRED);
+	}
+	
+	public void success(String msg) {
+		warnings.setText(msg);
+	}
+	
+	public void setWarning(String text) {
+		warnings.setText(text == null || text.trim().isEmpty() ? "-- All Clear --": text);
 	}
 }
